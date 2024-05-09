482eb10289359d9f2f9fe774cddcd3ee784cbeac
Morterolath <ayberkk0705@gmail.com>
2024-05-09 12:59:08 +0300

    initial commit

---
+++ ArtPulseAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using ArtPulseAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtPulseAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        internal DbSet<Customer> Customers { get; set; }
+        internal DbSet<Offer> Offers { get; set; }
+        internal DbSet<Product> Products { get; set; }
+        internal DbSet<Seller> Sellers { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
