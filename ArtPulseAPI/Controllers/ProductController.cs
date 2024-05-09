0b5f3de0d542cfce3d69deaa829837c417498f5d
Morterolath <ayberkk0705@gmail.com>
2024-05-09 17:29:55 +0300

    new commit

--- ArtPulseAPI/Controllers/ProductController.cs
+++ ArtPulseAPI/Controllers/ProductController.cs
@@ -19,96 +19,42 @@ namespace ArtPulseAPI.Controllers
         public IActionResult GetProductsPriceAsc(int takeCount)
         {
             Product[] retreivedProducts = _dataContext.Products.OrderBy(p => p.Cost).Take(takeCount).ToArray();
-            ProductDTO[] productDtoArray = new ProductDTO[retreivedProducts.Length];
-            for (int i = 0; i < retreivedProducts.Length; i++)
-            {
-                Product product = retreivedProducts[i];
-                productDtoArray[i] = new ProductDTO()
-                {
-                    SellerId = product.Seller.Id,
-                    Amount = product.Amount,
-                    Category = "placeholder category",
-                    Cost = product.Cost,
-                    Details = product.Details,
-                    Id = product.Id,
-                    Name = product.Name,
-                    RatingScaledBy10 = product.RatingScaledBy10
-                };
-            }
-            return Ok(productDtoArray);
+            return Ok(ProductToProductDTO(retreivedProducts));
         }
         [HttpGet("/priceDesc/{takeCount}")]
         public IActionResult GetProductsPriceDesc(int takeCount)
         {
             Product[] retreivedProducts = _dataContext.Products.OrderByDescending(p => p.Cost).Take(takeCount).ToArray();
-            ProductDTO[] productDtoArray = new ProductDTO[retreivedProducts.Length];
-            for (int i = 0; i < retreivedProducts.Length; i++)
-            {
-                Product product = retreivedProducts[i];
-                productDtoArray[i] = new ProductDTO()
-                {
-                    SellerId = product.Seller.Id,
-                    Amount = product.Amount,
-                    Category = "placeholder category",
-                    Cost = product.Cost,
-                    Details = product.Details,
-                    Id = product.Id,
-                    Name = product.Name,
-                    RatingScaledBy10 = product.RatingScaledBy10
-                };
-            }
-            return Ok(productDtoArray);
+            return Ok(ProductToProductDTO(retreivedProducts));
         }
 
         [HttpGet("/{takeCount}/{category}")]
         public IActionResult GetBestProducts(int takeCount, Category category)
         {
             Product[] retreivedProducts = _dataContext.Products.Where(p=> p.Category == category).OrderByDescending(p => p.RatingScaledBy10).Take(takeCount).ToArray();
-            ProductDTO[] productDtoArray = new ProductDTO[retreivedProducts.Length];
-            for (int i = 0; i < retreivedProducts.Length; i++)
-            {
-                Product product = retreivedProducts[i];
-                productDtoArray[i] = new ProductDTO()
-                {
-                    SellerId = product.Seller.Id,
-                    Amount = product.Amount,
-                    Category = "placeholder category",
-                    Cost = product.Cost,
-                    Details = product.Details,
-                    Id = product.Id,
-                    Name = product.Name,
-                    RatingScaledBy10 = product.RatingScaledBy10
-                };
-            }
-            return Ok(productDtoArray);
+            return Ok(ProductToProductDTO(retreivedProducts));
         }
         [HttpGet("/{takeCount}")]
         public IActionResult GetBestProducts(int takeCount)
         {
             Product[] retreivedProducts = _dataContext.Products.OrderByDescending(p => p.RatingScaledBy10).Take(takeCount).ToArray();
-            ProductDTO[] productDtoArray = new ProductDTO[retreivedProducts.Length];
-            for(int i = 0; i < retreivedProducts.Length; i++)
+            if(retreivedProducts.Length == 0)
             {
-                Product product = retreivedProducts[i];
-                productDtoArray[i] = new ProductDTO()
-                {
-                    SellerId = product.Seller.Id,
-                    Amount = product.Amount,
-                    Category = "placeholder category",
-                    Cost = product.Cost,
-                    Details = product.Details,
-                    Id = product.Id,
-                    Name = product.Name,
-                    RatingScaledBy10 = product.RatingScaledBy10
-                };
+                return NotFound();
             }
-            return Ok(productDtoArray);
+            return Ok(ProductToProductDTO(retreivedProducts));
         }
 
         [HttpGet]
         public IActionResult GetProducts()
         {
-            Product[] products = _dataContext.Products.ToArray();
+            Product[] retreivedProducts = _dataContext.Products.ToArray();
+            return Ok(ProductToProductDTO(retreivedProducts));
+        }
+
+
+        ProductDTO[] ProductToProductDTO(Product[] products)
+        {
             ProductDTO[] productDTOArray = new ProductDTO[products.Length];
             for (int i = 0; i < products.Length; i++)
             {
@@ -125,7 +71,7 @@ namespace ArtPulseAPI.Controllers
                     RatingScaledBy10 = product.RatingScaledBy10
                 };
             }
-            return Ok(productDTOArray);
+            return productDTOArray;
         }
     }
 }
---
+++ ArtPulseAPI/Migrations/20240509111438_InitialMigration.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ArtPulseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OfferName = table.Column<string>(type: "text", nullable: false),
+                    OfferDescription = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    RatingScaledBy10 = table.Column<int>(type: "integer", nullable: false),
+                    Category = table.Column<int>(type: "integer", nullable: false),
+                    Cost = table.Column<decimal>(type: "numeric", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Details = table.Column<string>(type: "text", nullable: false),
+                    SellerId = table.Column<int>(type: "integer", nullable: false),
+                    OfferId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Offers_OfferId",
+                        column: x => x.OfferId,
+                        principalTable: "Offers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Products_Sellers_SellerId",
+                        column: x => x.SellerId,
+                        principalTable: "Sellers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerProduct",
+                columns: table => new
+                {
+                    CustomersWithThisProductInShoppingCartId = table.Column<int>(type: "integer", nullable: false),
+                    ProductsInShoppingCartId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerProduct", x => new { x.CustomersWithThisProductInShoppingCartId, x.ProductsInShoppingCartId });
+                    table.ForeignKey(
+                        name: "FK_CustomerProduct_Customers_CustomersWithThisProductInShoppin~",
+                        column: x => x.CustomersWithThisProductInShoppingCartId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CustomerProduct_Products_ProductsInShoppingCartId",
+                        column: x => x.ProductsInShoppingCartId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerProduct_ProductsInShoppingCartId",
+                table: "CustomerProduct",
+                column: "ProductsInShoppingCartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OfferId",
+                table: "Products",
+                column: "OfferId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SellerId",
+                table: "Products",
+                column: "SellerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerProduct");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Offers");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
