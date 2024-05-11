4ec39e372f9c139a233a6f2f1df5b73ff28c8e53
Omer Akgeyik <omer.akgeyik@outlook.com>
2024-05-11 12:37:46 +0300

    update

--- ArtPulseAPI/obj/Debug/net8.0/ArtPulseAPI.AssemblyInfo.cs
+++ ArtPulseAPI/obj/Debug/net8.0/ArtPulseAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtPulseAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1ccfba6a6a12a8fbf0c35dd2246fcc0cc9e32a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtPulseAPI")]
